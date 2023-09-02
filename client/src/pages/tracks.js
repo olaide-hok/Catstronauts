07ef89d5b6308795b9fb4a5383ce4a6a117b5b5f
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-09-02 14:40:51 +0100

    Add TRACKS query wrapped with gql

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from 'react'
+import {gql} from '@apollo/client'
+import {Layout} from '../components'
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
-const Tracks = () => {
-  return <Layout grid> </Layout>;
-};
 
-export default Tracks;
+/** TRACKS query to retrieve all tracks */
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`
+
+const Tracks = () => {
+    return <Layout grid> </Layout>
+}
+
+export default Tracks
