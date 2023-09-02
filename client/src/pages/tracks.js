d3954a8f3a090eba3920293b85028e6e4076f55f
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-09-02 18:40:31 +0100

    Complete Tracks component.

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import {gql} from '@apollo/client'
+import {useQuery, gql} from '@apollo/client'
+import TrackCard from '../containers/track-card'
 import {Layout} from '../components'
 
-/**
- * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
-
-/** TRACKS query to retrieve all tracks */
+/** TRACKS gql query to retrieve all tracks */
 const TRACKS = gql`
   query GetTracks {
     tracksForHome {
@@ -25,8 +21,22 @@ const TRACKS = gql`
   }
 `
 
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
 const Tracks = () => {
-    return <Layout grid> </Layout>
+    const {loading, error, data} = useQuery(TRACKS)
+
+    return (
+        <Layout grid>
+            <QueryResult error={error} loading={loading} data={data}>
+                {data?.tracksForHome?.map((track) => (
+                    <TrackCard key={track.id} track={track} />
+                ))}
+            </QueryResult>
+        </Layout>
+    )
 }
 
 export default Tracks
